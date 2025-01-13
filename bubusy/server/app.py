3098d981299ec1bb9babfc1aa473ce15754a5ebd
tzinyongo <51459077+tzinyongo@users.noreply.github.com>
2025-01-12 20:48:13 -0500

    new changes

--- bubusy/server/app.py
+++ bubusy/server/app.py
@@ -30,9 +30,13 @@ def load_data():
 def predict_density():
     df = load_data()
     df = preprocess_data(df)
-    X_vars = df[['real_time_hour', 'building_floor_2', 'building_floor_3', 'building_floor_1']]
-    df['real_time_density_cnt'] = model.predict(X_vars)
-    return jsonify(df[['date', 'hour', 'real_time_density_cnt']].to_dict(orient='records'))
+    most_recent = df.iloc[-1]['date'] 
+    best_df = best_df.rename(columns={'hour':'past_hour', 'density_cnt':'past_hour_density_cnt', 'real_time_hour':'hour'})
+    X_vars = best_df[['hour', 'building_floor_2', 'building_floor_3', 'building_floor_l']]
+    best_df['real_time_density_cnt'] = best_model.predict(X_vars).round(0)
+    best_df  
+    predictions = df[['real_time_hour', 'buidling_floor', 'real_time_density_cnt']].to_dict(orient='records')
+    return jsonify(predictions)
 
 if __name__ == '__main__':
     app.run(debug=True)
