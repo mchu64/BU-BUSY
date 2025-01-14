b828bdb92e3489c35af11db7bdddc9c7b0638c63
Kevjbr <157067782+Kevjbr@users.noreply.github.com>
2025-01-14 15:51:07 -0500

    test to see app.py saves

--- bubusy/server/app.py
+++ bubusy/server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from sqlalchemy import create_engine
 import pandas as pd
 import joblib
 from utils.preprocessing import preprocess_data
 
 app = Flask(__name__)
+CORS(app)
 
 # Load model
 model = joblib.load('./models/xgboost_model.pkl')
@@ -23,7 +25,8 @@ def load_data():
                FROM public.densitymap_hourly 
                WHERE building_desc = '915 COMMONWEALTH AVENUE' AND hour >= 6 AND hour <=23 
                AND date >= '2024-09-03' 
-               ORDER BY date ASC;"""
+               ORDER BY date ASC
+               ORDER BY date ASC, hour ASC;"""
     return pd.read_sql(query, engine)
 
 @app.route('/predict', methods=['POST'])
@@ -32,8 +35,9 @@ def predict_density():
     df = preprocess_data(df)
     X_vars = df[['hour', 'building_floor_2', 'building_floor_3', 'building_floor_l']]
     df['real_time_density_cnt'] = model.predict(X_vars).round(0)
-    most_recent = df.iloc[[-1]] 
-    predictions = most_recent[['hour', 'buidling_floor', 'real_time_density_cnt']].to_dict(orient='records')
+    most_recent_date = df[df['date'] == df.iloc[-1]['date']]
+    most_recent_hour = most_recent_date[most_recent_date['hour'] == df.iloc[-1]['hour']]
+    predictions = most_recent_hour[['hour', 'building_floor', 'real_time_density_cnt']].to_dict(orient='records')
     return jsonify(predictions)
 
 if __name__ == '__main__':
